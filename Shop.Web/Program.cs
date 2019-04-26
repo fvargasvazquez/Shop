83e07cd9930665a4841d8d86bb43cb10be7f5e00
Francisco Vargas Vazquez <fvargasvazquez@hotmail.com>
2019-04-25 19:39:44 -0500

    SEED DB

--- Shop.Web/Program.cs
+++ Shop.Web/Program.cs
@@ -6,19 +6,45 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Shop.Web.Data.Entities;
 
 namespace Shop.Web
 {
+    //public class Program
+    //{
+    //    public static void Main(string[] args)
+    //    {
+    //        CreateWebHostBuilder(args).Build().Run();
+    //    }
+
+    //    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+    //        WebHost.CreateDefaultBuilder(args)
+    //            .UseStartup<Startup>();
+    //}
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            RunSeeding(host);
+            host.Run();
+        }
+
+        private static void RunSeeding(IWebHost host)
+        {
+            var scopeFactory = host.Services.GetService<IServiceScopeFactory>();
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var seeder = scope.ServiceProvider.GetService<SeedDb>();
+                seeder.SeedAsync().Wait();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
     }
+
 }
